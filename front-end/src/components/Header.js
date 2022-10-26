27bda288f7ac98eb5b2ce63e72844ae912621e2e
Aïssetou SACKO <aissetou.sck@gmail.com>
2022-10-26 21:43:50 +0200

    Home page

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/argentBankLogo.png'
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="main-nav">
+        <Link to="/" className="main-nav-logo">
+          <img
+            className="main-nav-logo-image"
+            src={logo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Link>
+
+        <Link to="/login" className="main-nav-item">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
